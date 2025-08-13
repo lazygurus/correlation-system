c4bca4eee7befbc30f9112ef7a5f6caa24cff7e1
hxy987 <3504776610@qq.com>
2025-08-13 10:49:04 +0800

    修改了高斯离散的广播机制 添加了一个检查初始数据第一列是否为字符串

--- core/distance.py
+++ core/distance.py
@@ -21,15 +21,17 @@ def gaussian_discretization_fast(df: pd.DataFrame, sigma: float = 1.0, bins: int
     return_zscore: True 返回离散等级，False 返回原值中心
     """
     data = df.to_numpy()
-    n_samples, n_features = data.shape
 
     # 每行均值与标准差
     mu = data.mean(axis=1, keepdims=True)
     std = data.std(axis=1, keepdims=True) + 1e-12
-
+    print(mu.shape, std.shape)
+    mu = mu.squeeze()  # (387,)
+    std = std.squeeze()  # (387,)
+    print(mu.shape, std.shape)
     # 在 z-score 空间生成中心点
     z_centers = np.linspace(-3, 3, bins)  # (bins,)
-    centers = mu + std * z_centers[:, None]  # shape: (bins, n_samples)
+    centers = mu[None, :]  + std[None, :]  * z_centers[:, None]  # shape: (bins, n_samples)
 
     # 广播计算权重
     # data: (n_samples, n_features)
@@ -81,6 +83,11 @@ def compute_distance_matrix(df: pd.DataFrame, method: str = "euclidean", sigma: 
         - "euclidean": 用标准化数据计算欧式距离
         - "information": 用离散化数据计算信息距离
     """
+    # 先检查第一列是否为字符串，如果是的话，就将其转换为行索引
+    first_col = df.iloc[:, 0]
+    if first_col.dtype == 'object' or isinstance(first_col.iloc[0], str):
+        df = df.set_index(df.columns[0])  # 把第一列作为索引
+
     if method == "euclidean":
         standardized_df = zscore_standardize_rows(df)
         return pairwise_distances(standardized_df, metric="euclidean")
@@ -89,3 +96,8 @@ def compute_distance_matrix(df: pd.DataFrame, method: str = "euclidean", sigma: 
         return information_distance(discretized_df)
     else:
         raise ValueError(f"未知的距离计算方法: {method}")
+
+if __name__ == "__main__":
+    df = pd.read_csv("D:\python\Relevance_System\data\statisticsData.txt" , encoding="gbk" , sep="\t" ,index_col=0)
+    pd = gaussian_discretization_fast(df)
+    print(pd)
